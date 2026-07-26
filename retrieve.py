6f971e63e020a0a4948905d4dc2795867b903342
v4g2b2v7rj-rgb <kushagrajais0224@gmail.com>
2026-07-26 15:47:40 +0530

    Enable testing of environment memory functions
    Uncommented code for testing environment memory retrieval.

--- retrieve.py
+++ retrieve.py
@@ -1,8 +1,8 @@
 import sqlite3
 from functions import env_format_memory, format_user_memory, get_all_env,search_env_memory
-
-# rows = get_all_env()
-# print(env_format_memory(rows))
+# for testing
+rows = get_all_env()
+print(env_format_memory(rows))
 thing = "macbook"
 search = search_env_memory(thing)
 print(search)
